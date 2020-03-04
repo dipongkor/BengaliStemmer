f466b9ab4a00ed0bac2a496ddd2462250de0299d
dipongkor <atishiitdu@hotmail.com>
2020-03-04 19:06:39 +0600

    all code converted

--- bengali_stemmer.py
+++ bengali_stemmer.py
@@ -50,7 +50,169 @@ def stripPluralSuffixes(word):
         word[word_length - 1] == bn_suffixes["bn_I"]:
         word = word[0: word_length - 4]
     return word
+
+def stripCommonSuffixes(word, i_removed):
+    new_len = len(word)
+    old_len = len(word)
+    while True:
+        if old_len <= 4:
+            break   
+        #Remove 'tta' or 'ta' (only if it is not preceeded with a m or g)
+        if word[old_len - 1] == bn_suffixes["bn_AA"] and \
+            word[old_len - 2] == bn_suffixes["bn_tt"] or \
+            word[old_len - 2] == bn_suffixes["bn_t"] and \
+            word[old_len - 3] != bn_suffixes["bn_m"] and \
+            word[old_len - 3] != bn_suffixes["bn_g"]:
+            word = word[0: old_len - 2]
+            old_len = len(word)
+
+            if old_len <= 4:
+                break
+
+        #Remove 'ti' or 'tti'
+        if word[old_len - 1] == bn_suffixes["bn_I"] and word[old_len - 2] == bn_suffixes["bn_tt"]:
+            word = word[0: old_len - 2]
+            old_len = len(word)
+
+            if old_len <= 4:
+                break
         
+        #Remove "ra"  ("rai" has alreday been stemmed to "ra")
+        if word[old_len - 1] == bn_suffixes["bn_r"]:
+            word = word[0: old_len - 1]
+            old_len = len(word)
+
+            if old_len <= 4:
+                break
+
+            #Remove "-er"
+            if word[old_len - 1] == bn_suffixes["bn_E"]:
+                position = 2 if word[old_len - 2] == bn_suffixes["bn_d"] else 1
+                word = word[0: old_len - position]
+                old_len = len(word)
+        
+        if old_len <= 5:
+            break
+
+        #Remove ttai tai ttar or tar
+        if word[old_len - 1] == bn_suffixes["bn_y"] or \
+            word[old_len - 1] == bn_suffixes["bn_r"] and \
+            word[old_len - 2] == bn_suffixes["bn_AA"] and \
+            word[old_len - 3] == bn_suffixes["bn_tt"] or \
+            word[old_len - 3] == bn_suffixes["bn_t"]:
+            word = word[0: old_len - 3]
+            old_len = len(word)
+        elif word[old_len - 1] == bn_suffixes["bn_r"] and word[old_len - 2] == bn_suffixes["bn_I"] and \
+            word[len - 3] == bn_suffixes["bn_tt"]:
+            word = word[0: old_len - 3]
+            old_len = len(word)
+
+        if old_len <= 5:
+            break
+
+        if word[old_len - 1] == bn_suffixes["bn_E"] and \
+            word[old_len - 2] == bn_suffixes["bn_k"]:
+            word = word[0: old_len - 2]
+            old_len = len(word)
+        
+        if old_len <= 5:
+            break
+
+        #Remove 'shil'
+        if word[old_len - 1] == bn_suffixes["bn_l"] and \
+            word[old_len - 2] == bn_suffixes["bn_II"] and \
+            word[old_len - 3] == bn_suffixes["bn_sh"]:
+            word = word[0: old_len - 3]
+            old_len = len(word)
+        
+        if old_len <= 6:
+            break
+
+        #Remove 'tuku'
+        if word[old_len - 1] == bn_suffixes["bn_U"] and \
+            word[old_len - 2] == bn_suffixes["bn_k"] and \
+            word[old_len - 3] == bn_suffixes["bn_U"] and \
+            word[old_len - 4] == bn_suffixes["bn_tt"]:
+            word = word[0: old_len - 4]
+            old_len = len(word)
+        
+        if old_len <= 6:
+            break
+
+        #Remove 'debi'
+        if word[old_len - 1] == bn_suffixes["bn_II"] and \
+            word[old_len - 2] == bn_suffixes["bn_b"] and \
+            word[old_len - 3] == bn_suffixes["bn_E"] and \
+            word[old_len - 4] == bn_suffixes["bn_d"]:
+            word = word[0: old_len - 4]
+            old_len = len(word)
+        
+        if old_len <= 6:
+            break
+
+        #Remove 'babu'
+        if word[old_len - 1] == bn_suffixes["bn_U"] and \
+            word[old_len - 2] == bn_suffixes["bn_b"] and \
+            word[old_len - 3] == bn_suffixes["bn_AA"] and \
+            word[old_len - 4] == bn_suffixes["bn_b"]:
+            word = word[0: old_len - 4]
+            old_len = len(word)
+        
+        if old_len <= 6 or not i_removed:
+            break
+
+        #Remove 'bhai'
+        if word[old_len - 1] == bn_suffixes["bn_AA"] and \
+            word[old_len - 2] == bn_suffixes["bn_bh"]:
+            word = word[0: old_len - 2]
+            old_len = len(word)
+        
+        if old_len <= 6:
+            break
+
+        #Remove 'bhabe'
+        if word[old_len - 1] == bn_suffixes["bn_b"] and \
+            word[old_len - 2] == bn_suffixes["bn_E"] and \
+            word[old_len - 3] == bn_suffixes["bn_AA"] and \
+            word[old_len - 4] == bn_suffixes["bn_bh"]:
+            word = word[0: old_len - 4]
+            old_len = len(word)
+    return word, new_len != old_len
+
+def stemWord(word):
+    new_len = len(word)
+    old_len = len(word)
+    i_removed = False
+    is_aggressive = False
+
+    buff = word
+
+    if not is_aggressive and old_len <= 3:
+        return buff
     
+    if buff[old_len -1] == bn_suffixes["bn_i"] or \
+        buff[old_len -1] == bn_suffixes["bn_o"]:
+        buff = buff[0: old_len -1]
+        old_len = old_len -1
     
-   
+    buff, changed_length = stripCommonSuffixes(buff, i_removed)
+
+    while changed_length:
+        buff, changed_length = stripCommonSuffixes(buff, i_removed)
+        i_removed = False
+    
+    buff = stripPluralSuffixes(buff)
+
+    if is_aggressive:
+        current_length = len(buff) - 1
+        while isBnCommonSuffix(buff[current_length]):
+            current_length = current_length - 1
+        if current_length - new_len + 1 >= 2:
+            buff = buff[0: current_length + 1]
+    
+    if buff[len(buff) - 1] == bn_suffixes["bn_E"]:
+        buff = buff[0: len(buff) - 1]
+    
+    return buff
+
+
--- test_bengali_stemmer.py
+++ test_bengali_stemmer.py
@@ -14,6 +14,21 @@ class TestBengaliStemmer(unittest.TestCase):
         self.assertEqual(stripPluralSuffixes('মানুষ'), 'মানুষ')
         self.assertEqual(stripPluralSuffixes('মানুষগুলোতে'), 'মানুষ')
         self.assertEqual(stripPluralSuffixes('মানুষগুলিতে'), 'মানুষ')
+    
+    def test_stripCommonSuffixes(self):
+        self.assertEqual(stripCommonSuffixes('মানুষগুলি', False), ('মানুষগুলি', False))
+        self.assertEqual(stripCommonSuffixes('মানুষটি', False), ('মানুষ', True))
+        self.assertEqual(stripCommonSuffixes('মানুষের', False), ('মানুষ', True))
+        self.assertEqual(stripCommonSuffixes('মানুষকে', False), ('মানুষ', True))
+        self.assertEqual(stripCommonSuffixes('মানুষদের', False), ('মানুষ', True))
+        self.assertEqual(stripCommonSuffixes('মানুষরা', False), ('মানুষরা', False))
+        self.assertEqual(stripCommonSuffixes('মরণশীল', False), ('মরণ', True))
+
+
+    def test_stemWord(self):
+        self.assertEqual(stemWord('মানুষগুলি'), 'মানুষ')
+        self.assertEqual(stemWord('পাখিরা'), 'পাখি')
+
 
 if __name__ == '__main__':
     unittest.main()
